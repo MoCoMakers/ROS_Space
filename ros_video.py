d9ee42548f9d62014573e4710c44c3325b4f8ff4
robogrow <just-money@comcast.net>
2016-11-03 17:26:23 -0400

    Add files via upload
    This is a bit better. find red, green, blue contours. Just get the average color inside the contours. returns corners of contours or just center pixel.
    
    Now we need the depth and imu head pose to get position.

--- ros_video.py
+++ ros_video.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+from scipy.spatial import distance as dist
+from collections import OrderedDict
 
 import roslaunch
 
@@ -8,7 +10,6 @@ uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 roslaunch.configure_logging(uuid)
 launch = roslaunch.parent.ROSLaunchParent(uuid, ["srcsim", "qual1.launch"])
  
-
 import subprocess,os
 
 import roslib
@@ -37,45 +38,17 @@ import time
 import sensor_msgs.msg
 from cv_bridge import CvBridge     
 
-def sendRightArmTrajectory():
-    msg = ArmTrajectoryRosMessage()
-
-    msg.robot_side = ArmTrajectoryRosMessage.RIGHT
-
-    msg = appendTrajectoryPoint(msg, 2.0, ZERO_VECTOR)
-    msg = appendTrajectoryPoint(msg, 3.0, ELBOW_BENT_UP)
-    msg = appendTrajectoryPoint(msg, 4.0, ZERO_VECTOR)
-
-    msg.unique_id = -1
-
-    rospy.loginfo('publishing right trajectory')
-    armTrajectoryPublisher.publish(msg)
-
-def appendTrajectoryPoint(arm_trajectory, time, positions):
-    if not arm_trajectory.joint_trajectory_messages:
-        arm_trajectory.joint_trajectory_messages = [copy.deepcopy(OneDoFJointTrajectoryRosMessage()) for i in range(len(positions))]
-    for i, pos in enumerate(positions):
-        point = TrajectoryPoint1DRosMessage()
-        point.time = time
-        point.position = pos
-        point.velocity = 0
-        arm_trajectory.joint_trajectory_messages[i].trajectory_points.append(point)
-    return arm_trajectory
-
-
 class Find_leds:
-
+    
     def __init__(self):
-        self.cnt=0
-        roi = cv2.imread('img.jpg')
-        #roi2 = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY) 
-
+	self.cnt=0
         # find all the 'black' shapes in the image
-        lower = np.array([0, 0, 0])
-        upper = np.array([5, 5, 5])
-        shapeMask = cv2.inRange(roi, lower, upper)
-        smblur = cv2.GaussianBlur(shapeMask, (9,9), 2, 2)
-        roi2 = cv2.Canny(smblur,100,200)
+        self.greenlower = np.array([0, 120, 0])
+        self.greenupper = np.array([20, 255, 20])
+        self.bluelower = np.array([120, 0, 0])
+        self.blueupper = np.array([255, 20, 20])
+        self.redlower = np.array([0, 0, 120])
+        self.redupper = np.array([20, 20, 255])
 
         self.NUM_JOINTS = 28
         # Latest message from /atlas/atlas_sim_interface_state
@@ -111,29 +84,50 @@ class Find_leds:
         #cv2.imshow('green',green)
         #img = cv2.imread('img.jpg')
         img1 = cv2.imread('img2.jpg')
-        img2 = img1.copy()
-
+        img2 = img.copy()
+	frame = img.copy()
+        img = cv2.flip(frame,-1)
+        #cv2.imshow('f',frame)
+ 
         # allocate temporary images
         #new_size = (int(img.shape[1] / image_scale), int(img.shape[0] / image_scale))
         
-	# find all the 'black' shapes in the image
-	lower = np.array([0, 0, 0])
-	upper = np.array([5, 5, 5])
-	shapeMask = cv2.inRange(img, lower, upper)
+	# find all the 'blue' shapes in the image
 
+	green = cv2.inRange(img, self.greenlower, self.greenupper)
+	blue = cv2.inRange(img, self.bluelower, self.blueupper)
+	red = cv2.inRange(img, self.redlower, self.redupper)
+        #cv2.bitwise_or(shapeMask,img,img2)
         # convert color input image to grayscale
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-        frame = cv2.GaussianBlur(gray, (9,9), 2, 2)
-        edges1 = cv2.Canny(gray,100,200)
-        smblur = cv2.GaussianBlur(shapeMask, (9,9), 2, 2)
+        #gray = cv2.cvtColor(shapeMask, cv2.COLOR_BGR2GRAY) 
+
+        #frame = cv2.GaussianBlur(green, (9,9), 2, 2)
+        grn = cv2.Canny(green,100,200)
+        blu = cv2.Canny(blue,100,200)
+        rd = cv2.Canny(red,100,200)
+        #both = cv2.bitwise_or(grn,blu)
+        cv2.imshow('red',rd)
+        cv2.imshow('green',grn)
+        cv2.imshow('blue',blu)
+        contours,h = cv2.findContours(red,1,2)
+        for ct in contours:
+            #print('approx',approx)
+	
+            #if len(approx)>2 and len(approx)<7:
+            #if cv2.contourArea(ct)>20 and cv2.isContourConvex(ct)==True:
+            if cv2.contourArea(ct)>60:
+                print('contour= ',ct)
+
+        smblur = cv2.GaussianBlur(red, (9,9), 2, 2)
         edges_sm = cv2.Canny(smblur,100,200)
  
         edges2 = cv2.Canny(frame,100,200) 
         cv2.imshow("console", img)
         #cv2.imshow("console lit", img1)
-        #  cv2.imshow("black shapes", shapeMask)
+	#cv2.imshow('sm1',roi2)img2)
+        #cv2.imshow("black shapes", img2)
         #cv2.imshow('outer edges',edges2)
-        #  cv2.imshow('all edges', edges1)
+        #cv2.imshow('all edges', red)
         #lights =cv2.medianBlur(edges1, 3)
         '''
         if cnt==0:
@@ -141,7 +135,7 @@ class Find_leds:
            roi1=img2.copy()
         #cv2.imshow('roi2',roi2)
         '''
-        cv2.imshow('edges_sm',edges_sm)
+        #cv2.imshow('edges_sm',edges_sm)
         
         if self.cnt>0:
 	   #cv2.imshow('sm1',roi2)
@@ -152,7 +146,7 @@ class Find_leds:
         rows,cols = edges_sm.shape
         roi2 = edges_sm[0:rows, 0:cols ]
 
-        ret,thresh = cv2.threshold(gray,127,255,1)
+        #ret,thresh = cv2.threshold(gray,127,255,1)
 
         contours,h = cv2.findContours(edges_sm,1,2)
 
@@ -221,3 +215,33 @@ if __name__ == '__main__':
 
     #rospy.Subscriber(options.ctopic, sensor_msgs.msg.CompressedImage, compressed_detect_and_draw)
     rospy.spin()       
+
+
+
+
+def sendHeadTrajectory():
+    msg = ArmTrajectoryRosMessage()
+
+    msg.robot_side = ArmTrajectoryRosMessage.RIGHT
+
+    msg = appendTrajectoryPoint(msg, 2.0, ZERO_VECTOR)
+    msg = appendTrajectoryPoint(msg, 3.0, ELBOW_BENT_UP)
+    msg = appendTrajectoryPoint(msg, 4.0, ZERO_VECTOR)
+
+    msg.unique_id = -1
+
+    rospy.loginfo('publishing right trajectory')
+    armTrajectoryPublisher.publish(msg)
+
+def appendTrajectoryPoint(arm_trajectory, time, positions):
+    if not arm_trajectory.joint_trajectory_messages:
+        arm_trajectory.joint_trajectory_messages = [copy.deepcopy(OneDoFJointTrajectoryRosMessage()) for i in range(len(positions))]
+    for i, pos in enumerate(positions):
+        point = TrajectoryPoint1DRosMessage()
+        point.time = time
+        point.position = pos
+        point.velocity = 0
+        arm_trajectory.joint_trajectory_messages[i].trajectory_points.append(point)
+    return arm_trajectory
+
+
